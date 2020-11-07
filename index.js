b56cf8fa0f5891cb210f9ac645b64f90f9dc0dad
witty-abstraction-1855 <merve.kastan4@gmail.com>
2020-11-07 22:02:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ function removeFromCart(item) {
 
 
 function placeOrder(cardNumber) {
-  if (cardNumber) {
+  if (cardNumber>0) {
     console.log(`Your total cost is $ ${total()}, which will be charged to the card ${cardNumber}.`);
      cart=[]
   }
