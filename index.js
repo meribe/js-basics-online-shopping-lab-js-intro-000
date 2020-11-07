b5650362ddd3526ce18b6e83d40be0a97579f162
witty-abstraction-1855 <merve.kastan4@gmail.com>
2020-11-07 22:00:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -60,10 +60,10 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (cardNumber) {
-    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+    console.log(`Your total cost is $ ${total()}, which will be charged to the card ${cardNumber}.`);
      cart=[]
   }
   else {
-      console.log("We don't have a credit card on file for you.");
+      console.log("Sorry, we don't have a credit card on file for you.");
   }
 }
